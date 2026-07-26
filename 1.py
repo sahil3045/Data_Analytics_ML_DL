3740109b7ce4ab1a3843c38a7a72a8e573a670d1
Sahil Kanchan <sahilkanchan3045@gmail.com>
2026-07-26 12:10:35 +0530

    .

---
+++ 1.py
@@ -0,0 +1,8 @@
+temperatures = [28.5, 33.7, 32.9, 31.6]
+
+total = 0
+for i in temperatures: 
+    total += i 
+
+avg = total/len(temperatures)
+print(avg)
---
+++ np_basics.py
@@ -0,0 +1,54 @@
+import numpy as np 
+
+temperatures = np.array([28.5, 33.7, 32.9, 31.6])
+average = np.mean(temperatures)
+print(average)
+
+#if you want to check the shape of an array 
+#print(temperatures.shape)
+
+#if you want to check the total no of elements of an array 
+#print(temperatures.size)
+
+#if you want to check the dimension of an array 
+#print(temperatures.ndim)
+
+'''
+#adv of numpys (numbers in python )
+#used to calculate data from millions of numbers in mili seconds 
+#1 speed 80 to 100 perc faster
+#2 less memory 
+#3 easy math operations 
+'''
+
+#one dimensional array 
+arr_1d = np.array([1, 2, 3, 4, 5])
+print(arr_1d)
+
+#two dimensional array 
+arr_2d = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+print(arr_2d)
+print(arr_2d.ndim)
+
+#default values array 
+zeroes_array = np.zeros(3) # here 3 is the size/shape that you want of your array 
+
+ones_arr = np.ones((2,3))
+print(ones_arr)
+
+#if you want custom value in place of zero or 1 
+full_arr = np.full((3,3), 9)
+print(full_arr)
+
+#creating sequences of numbers in umpy 
+#arange(start, stop, step )
+
+arrr = np.arange(1, 10, 3)
+print(arrr)
+
+#if you want to create identity matrix 
+im = np.eye(4)
+print(im)
+
